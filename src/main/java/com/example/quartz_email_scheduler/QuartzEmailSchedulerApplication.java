314a855667a8d880c09e29c05f8253900b7d33fd
Mohanraam-V-K <mohanraam.vk@prodapt.com>
2023-10-17 10:38:52 +0530

    first commit

---
+++ src/main/java/com/example/quartz_email_scheduler/QuartzEmailSchedulerApplication.java
@@ -0,0 +1,15 @@
+package com.example.quartz_email_scheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+
+@SpringBootApplication
+public class QuartzEmailSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuartzEmailSchedulerApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/quartz_email_scheduler/controller/EmailJobSchController.java
@@ -0,0 +1,92 @@
+package com.example.quartz_email_scheduler.controller;
+
+
+import org.quartz.JobBuilder;
+import org.quartz.JobDataMap;
+import org.quartz.JobDetail;
+import org.quartz.Scheduler;
+import org.quartz.SchedulerException;
+import org.quartz.SimpleScheduleBuilder;
+import org.quartz.Trigger;
+import org.quartz.TriggerBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.quartz_email_scheduler.jobs.EmailJob;
+import com.example.quartz_email_scheduler.payload.ScheduleEmailReq;
+import com.example.quartz_email_scheduler.payload.ScheduleEmailRes;
+
+import jakarta.validation.Valid;
+import java.time.ZonedDateTime;
+import java.util.Date;
+import java.util.UUID;
+
+@RestController
+@CrossOrigin
+@RequestMapping("api/v1/emailSchedular")
+public class EmailJobSchController {
+    private static final Logger logger = LoggerFactory.getLogger(EmailJobSchController.class);
+
+    @Autowired
+    private Scheduler scheduler;
+
+    @PostMapping("/scheduleEmail")
+    public ResponseEntity<ScheduleEmailRes> scheduleEmail(@Valid @RequestBody ScheduleEmailReq scheduleEmailRequest) {
+        try {
+            ZonedDateTime dateTime = ZonedDateTime.of(scheduleEmailRequest.getDateTime(), scheduleEmailRequest.getTimeZone());
+            if(dateTime.isBefore(ZonedDateTime.now())) {
+                ScheduleEmailRes scheduleEmailResponse = new ScheduleEmailRes(false,
+                        "dateTime must be after current time");
+                return ResponseEntity.badRequest().body(scheduleEmailResponse);
+            }
+
+            JobDetail jobDetail = buildJobDetail(scheduleEmailRequest);
+            Trigger trigger = buildJobTrigger(jobDetail, dateTime);
+            scheduler.scheduleJob(jobDetail, trigger);
+
+            ScheduleEmailRes scheduleEmailResponse = new ScheduleEmailRes(true,
+                    jobDetail.getKey().getName(), jobDetail.getKey().getGroup(), "Email Scheduled Successfully!");
+            return ResponseEntity.ok(scheduleEmailResponse);
+        } catch (SchedulerException ex) {
+            logger.error("Error scheduling email", ex);
+
+            ScheduleEmailRes scheduleEmailResponse = new ScheduleEmailRes(false,
+                    "Error scheduling email. Please try later!");
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(scheduleEmailResponse);
+        }
+    }
+
+    private JobDetail buildJobDetail(ScheduleEmailReq scheduleEmailRequest) {
+        JobDataMap jobDataMap = new JobDataMap();
+
+        jobDataMap.put("email", scheduleEmailRequest.getEmail());
+        jobDataMap.put("subject", scheduleEmailRequest.getSubject());
+        jobDataMap.put("body", scheduleEmailRequest.getBody());
+
+        return JobBuilder.newJob(EmailJob.class)
+                .withIdentity(UUID.randomUUID().toString(), "email-jobs")
+                .withDescription("Send Email Job")
+                .usingJobData(jobDataMap)
+                .storeDurably()
+                .build();
+    }
+
+    private Trigger buildJobTrigger(JobDetail jobDetail, ZonedDateTime startAt) {
+        return TriggerBuilder.newTrigger()
+                .forJob(jobDetail)
+                .withIdentity(jobDetail.getKey().getName(), "email-triggers")
+                .withDescription("Send Email Trigger")
+                .startAt(Date.from(startAt.toInstant()))
+                .withSchedule(SimpleScheduleBuilder.simpleSchedule().withMisfireHandlingInstructionFireNow())
+                .build();
+    }
+
+}
